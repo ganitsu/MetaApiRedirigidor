5beccfe43f66de763b1c8ea2ceca4362be5fef58
nitsuga2 <giulianiagustin7@gmail.com>
2024-09-22 18:54:30 -0300

    port changed

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def ask():
     return jsonify({'response': response["message"], 'instance_id': instance_id})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=2050, debug=True)
